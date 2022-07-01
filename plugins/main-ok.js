c8145f9b5a68efcd8a1586c5b8e4d75af3e0e65e
Alezonty <99535100+Alezonty@users.noreply.github.com>
2022-07-01 21:44:29 +0800

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
 }
 
-handler.customPrefix = /^(ok)$/i
+handler.customPrefix = /^(bot)$/i
 handler.command = new RegExp
 
 module.exports = handler
